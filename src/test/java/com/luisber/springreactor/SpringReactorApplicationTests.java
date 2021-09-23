5ca44d26326fc1ca91ed0040d23178b7d61458e1
Luis Betancourt <luisber5330@gmail.com>
2021-09-23 08:33:30 -0500

    feat: add example test webclient

--- src/test/java/com/luisber/springreactor/SpringReactorApplicationTests.java
+++ src/test/java/com/luisber/springreactor/SpringReactorApplicationTests.java
@@ -2,7 +2,14 @@ package com.luisber.springreactor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class SpringReactorApplicationTests {
@@ -14,6 +21,22 @@ class SpringReactorApplicationTests {
     void simpleFluxExample() {
         Flux<String> fluxColors = Flux.just("red", "green", "blue");
         fluxColors.subscribe(System.out::println);
+    }
+
+    @Test
+    void exampleWebClient(){
+        String query="name:gardevoir (subtypes:mega OR subtypes:vmax)";
+        var client = WebClient.create("https://api.pokemontcg.io")
+                .method(HttpMethod.GET)
+                .uri(uriBuilder -> uriBuilder.path("/v2/cards")
+                        .queryParam("q", "{query}")
+                        .build(query))
+                .header("Accept", "application/json, text/plain, */*")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        client.subscribe(System.out::println);
+        StepVerifier.create(client).expectNextCount(1).verifyComplete();
     }
 
 }
